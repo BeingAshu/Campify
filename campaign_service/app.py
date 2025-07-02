2f38ded4f7359ecd277abc46e10ee8d188cd5fed
Ashutosh Jatale <79712095+BeingAshu@users.noreply.github.com>
2025-07-02 16:50:33 +0530

    added app.py

--- campaign_service/app.py
+++ campaign_service/app.py
@@ -1,1 +1,24 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+campaigns = []
+
+@app.route('/campaigns', methods=['GET'])
+def get_campaigns():
+    return jsonify(campaigns)
+
+@app.route('/campaigns', methods=['POST'])
+def add_campaign():
+    data = request.get_json()
+    campaign = {
+        "id": len(campaigns) + 1,
+        "name": data.get("name"),
+        "date": data.get("date")
+    }
+    campaigns.append(campaign)
+    return jsonify({"message": "Campaign added", "campaign": campaign}), 201
+
+if __name__ == '__main__':
+    app.run(port=5001)
+  
